43b741e6f92ce2ed1afb61c84032e7f40934d842
Bo Chipev <bo.chipev@protonmail.com>
2024-09-01 07:26:12 +0200

    config to build a github pages site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://bchipev.github.io',
+    base: 'alphabet-audio-app',
+});
